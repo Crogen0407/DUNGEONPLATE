c2a8e32b893a5c2367a46be2726a64b8055ae560
Crogen0407 <mymcoding0407@gmail.com>
2024-11-18 23:39:44 +0900

    feat : AttackDirArrowVisual

--- 2024_winapigamep_framework_22/AttackDirArrow.cpp
+++ 2024_winapigamep_framework_22/AttackDirArrow.cpp
@@ -2,6 +2,8 @@
 #include "AttackDirArrow.h"
 #include "GDISelector.h"
 #include "Object.h"
+#include "InputManager.h"
+
 AttackDirArrow::AttackDirArrow()
 	: parent(nullptr)
 {
@@ -19,11 +21,21 @@ void AttackDirArrow::SetParent(Object* parent)
 void AttackDirArrow::Update()
 {
 	if (parent == nullptr) return;
-	SetPos({ parent->GetPos().x, parent->GetPos().y - 30 });
+
+	Vec2 mousePos = GET_MOUSEPOS;
+	Vec2 attackDir = mousePos - parent->GetPos();
+	attackDir.Normalize();
+
+	float distance = 30;
+
+	Vec2 selfPos = (attackDir * distance) + parent->GetPos();
+
+	SetPos(selfPos);
 }
 
 void AttackDirArrow::Render(HDC _hdc)
 {
-	GDISelector(_hdc, PEN_TYPE::BLUE);
-	::RECT_RENDER(_hdc, GetPos().x, GetPos().y, 15, 15);
+	GDISelector gdi = GDISelector(_hdc, BRUSH_TYPE::RED);
+	GDISelector gdi2 = GDISelector(_hdc, PEN_TYPE::RED);
+	::ELLIPSE_RENDER(_hdc, GetPos().x, GetPos().y, 15, 15);
 }
