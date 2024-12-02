d34aa26230fb271be99356da57e185c1a56e3483
Crogen0407 <mymcoding0407@gmail.com>
2024-12-02 18:57:34 +0900

    feat : AttackRange

--- 2024_winapigamep_framework_22/AttackRange.cpp
+++ 2024_winapigamep_framework_22/AttackRange.cpp
@@ -1,2 +1,31 @@
 #include "pch.h"
 #include "AttackRange.h"
+#include "SpriteRenderer.h"
+
+AttackRange::AttackRange()
+{
+	AddComponent<SpriteRenderer>();
+	_spriteRenderer = GetComponent<SpriteRenderer>();
+	_spriteRenderer->SetTexture(L"AttackRange", L"Texture\\AttackRange.bmp");
+	_spriteRenderer->enable = true;
+	SetSize({ 110, 110 });
+}
+
+AttackRange::~AttackRange()
+{
+}
+
+void AttackRange::Update()
+{
+	SetPos(parent->GetPos());
+}
+
+void AttackRange::Render(HDC _hdc)
+{
+	ComponentRender(_hdc);
+}
+
+void AttackRange::SetDir(Vec2 dir)
+{
+	_spriteRenderer->LookAt(dir);
+}
