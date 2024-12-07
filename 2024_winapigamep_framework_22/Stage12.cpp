b056d4961fc343f8d37e56e7006cdcf44f1f24b5
Crogen0407 <mymcoding0407@gmail.com>
2024-12-07 21:13:14 +0900

    fix : 12Stage

--- 2024_winapigamep_framework_22/Stage12.cpp
+++ 2024_winapigamep_framework_22/Stage12.cpp
@@ -22,8 +22,8 @@ void Stage12::Init()
     AddBackground<BackgroundNull>(0, 1);
     AddBackground<BackgroundNull>(0, 2);
     AddBackground<BackgroundNull>(1, 0);
-    AddBackground<BackgroundA>(1, 1);
-    AddBackground<BackgroundBoss>(1, 2);
+    AddBackground<BackgroundBoss>(1, 1);
+    AddBackground<BackgroundA>(1, 2);
     AddBackground<BackgroundB>(2, 0);
     AddBackground<BackgroundA>(2, 1, true);
     AddBackground<BackgroundB>(2, 2);
